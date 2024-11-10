13d67ca23a7660d75bca6e9d2eff0451b128e8de
Alden Budiman <aldenbudiman@gmail.com>
2024-11-11 01:17:04 +0800

    Doctor Mostly Completed except recordAppointmentOutcome() method. Minor changes to OutcomeRecord and MedicalRecord classes. Completed Treatment and Diagnosis Class

--- hmsProject/src/hmsProject/Diagnosis.java
+++ hmsProject/src/hmsProject/Diagnosis.java
@@ -1,4 +1,50 @@
 package hmsProject;
 
-public class Diagnosis {
+import java.io.Serializable;
+
+public class Diagnosis implements Serializable {
+    private String description;
+    private String diagnosisDate;
+    private String diagnosedBy;
+    private String severity;
+    
+    public Diagnosis(String description, String diagnosisDate, String diagnosedBy, String severity) {
+        this.description = description;
+        this.diagnosisDate = diagnosisDate;
+        this.diagnosedBy = diagnosedBy;
+        this.severity = severity;
+    }
+    
+    // Getters and Setters
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getDiagnosisDate() {
+        return diagnosisDate;
+    }
+    
+    public void setDiagnosisDate(String diagnosisDate) {
+        this.diagnosisDate = diagnosisDate;
+    }
+    
+    public String getDiagnosedBy() {
+        return diagnosedBy;
+    }
+    
+    public void setDiagnosedBy(String diagnosedBy) {
+        this.diagnosedBy = diagnosedBy;
+    }
+    
+    public String getSeverity() {
+        return severity;
+    }
+    
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
 }
--- hmsProject/src/hmsProject/OutcomeRecord.java
+++ hmsProject/src/hmsProject/OutcomeRecord.java
@@ -4,55 +4,62 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class OutcomeRecord implements Serializable {
-	private String dateOfAppointment;
-	private String serviceProvided;
-	private ArrayList<prescribedMed> meds;
-	private String consultationNote;
-	public OutcomeRecord() {
-		
-	}
-	
-	public OutcomeRecord(Appointment appt) {
-		this.dateOfAppointment=appt.getDate();
-		this.serviceProvided=null;
-		this.meds=null;
-		this.consultationNote=null;
-	}
-	
-	
-	public static OutcomeRecord createOutcomeRecord(Appointment cmptAppt) {
-		// TODO - implement OutcomeRecord.createOutcomeRecord
-		OutcomeRecord newRecord= new OutcomeRecord(cmptAppt);
-		Scanner sc=new Scanner(System.in);
-		newRecord.setServiceProvided(sc);
-		newRecord.setMeds(sc);
-		newRecord.writeNote(sc);
-		return newRecord;
-	}
-	
-	public void writeNote(Scanner sc) {
-		// TODO Auto-generated method stub
-		System.out.println("What are the notes for this appointment?");
-		this.consultationNote=sc.nextLine();
-	}
+    private String dateOfAppointment;
+    private String serviceProvided;
+    private ArrayList<prescribedMed> meds;
+    private String consultationNote;
 
-	public void setMeds(Scanner sc) {
-		// TODO Auto-generated method stub
-		System.out.println("What are the medicine(s) to be prescribed?\nEnter 1 to add new Medicine\n Enter 0 when done");
-		while(true) {
-			if(sc.nextLine()=="1") { //should be changed to selection from list of available medicine to create prescribed med
-				this.meds.add(prescribedMed.createPrescribedMed(sc));
-			}
-			else {
-				return;
-			}
-			
-		}
-	}
-	
-	public void setServiceProvided(Scanner sc) {
-		System.out.println("What is the service provided?");
-		this.serviceProvided=sc.nextLine();
-	}
-	
+    public OutcomeRecord() {
+        this.meds = new ArrayList<prescribedMed>();
+    }
+    
+    public OutcomeRecord(Appointment appt) {
+        this.dateOfAppointment = appt.getDate();
+        this.serviceProvided = null;
+        this.meds = new ArrayList<prescribedMed>();
+        this.consultationNote = null;
+    }
+    
+    public static OutcomeRecord createOutcomeRecord(Appointment cmptAppt) {
+        OutcomeRecord newRecord = new OutcomeRecord(cmptAppt);
+        Scanner sc = new Scanner(System.in);
+        newRecord.setServiceProvided(sc);
+        newRecord.setMeds(sc);
+        newRecord.writeNote(sc);
+        return newRecord;
+    }
+    
+    public void writeNote(Scanner sc) {
+        System.out.println("What are the notes for this appointment?");
+        this.consultationNote = sc.nextLine();
+    }
+
+    public void setMeds(Scanner sc) {
+    	prescribedMed newMed = prescribedMed.createPrescribedMed(sc);
+        if(newMed != null) {
+            this.meds.add(newMed);
+        }
+    }
+    
+    public void setServiceProvided(Scanner sc) {
+        System.out.println("What is the service provided?");
+        this.serviceProvided = sc.nextLine();
+    }
+
+    // Getters
+    public String getDateOfAppointment() {
+        return dateOfAppointment;
+    }
+
+    public String getServiceProvided() {
+        return serviceProvided;
+    }
+
+    public ArrayList<prescribedMed> getMeds() {
+        return meds;
+    }
+
+    public String getConsultationNote() {
+        return consultationNote;
+    }
 }
