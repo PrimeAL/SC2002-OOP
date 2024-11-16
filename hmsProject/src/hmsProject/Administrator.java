db0206fb4b448d873620e8b156aaff27ed655f70
wjhan785 <143602749+wjhan785@users.noreply.github.com>
2024-11-16 12:34:47 +0800

    Small minor corrections

--- hmsProject/src/hmsProject/Administrator.java
+++ hmsProject/src/hmsProject/Administrator.java
@@ -2,11 +2,22 @@ package hmsProject;
 
 import java.util.Scanner;
 
+/**
+ * Administrator class that inherits from User class
+ */
 public class Administrator extends User {
 	private String name;
 	private String gender;
 	private int age;
 
+	/**
+	 * Administrator class constructor
+	 * @param uid
+	 * @param pw
+	 * @param name
+	 * @param gender
+	 * @param age
+	 */
 	public Administrator(String uid, String pw, String name, String gender, int age) {
 		super(uid, pw);
 		this.name=name;
@@ -16,10 +27,9 @@ public class Administrator extends User {
 	}
 
 	/**
-	 * 
-	 * @param apptSys
-	 * @param inven
-	 * @param users
+	 * Overrides userInterface method from User class to use Administrator-specific interface
+	 * @param adminCont
+	 * @param sc
 	 */
 	public void userInterface(AdminController adminCont,Scanner sc) {
 		// TODO - implement Administrator.userInterface
@@ -27,12 +37,18 @@ public class Administrator extends User {
 			System.out.println("Date:"+appt.getDate()+"|Time:"+appt.getTime()+"|Status:"+appt.getStatus());
 		} 
 	}
-	
+
+	/**
+	 * ToFill
+	 */
 	public void manageStaff() {
 		// TODO - implement Administrator.manageStaff
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * ToFill
+	 */
 	public void apptOp() {
 		// TODO - implement Administrator.apptOp
 		throw new UnsupportedOperationException();
